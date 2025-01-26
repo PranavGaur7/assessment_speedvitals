5f3826397af79de38f55794d490fd6dbd29d6fda
PranavGaur7 <raghavgau7@gmail.com>
2025-01-26 19:23:54 +0530

    aa

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = ({isDark}:{isDark:Boolean}) => {
     return (
-        <div className={ `${isDark?"bg-dark-primary border-dark-icons text-white":"border-gray-300"} w-full h-24 border-t-2  float-end flex items-center justify-center text-lg sm:text-xl font-bold text-gray-600`}>
+        <div className={ `${isDark?"bg-dark-primary border-dark-icons text-white":"border-gray-300"} px-8 w-full h-24 border-t-2  float-end flex items-center justify-center text-lg sm:text-xl font-bold text-gray-600`}>
             <span>
                 Copyright © 2025
             </span>
